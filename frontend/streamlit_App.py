d710c61ef617705694d6a821641cea9878036fcb
sinni <nitinsi.biz@outlook.com>
2025-04-07 11:39:15 +0530

    Update fake-news-detector-service

--- frontend/streamlit_App.py
+++ frontend/streamlit_App.py
@@ -11,7 +11,7 @@ text = st.text_area("Enter the news content:", height=200)
 if st.button("Detect"):
     if title and text:
         with st.spinner("Analyzing..."):
-            response = requests.post("http://0.0.0.0:8000/predict", json={"title": title, "text": text})
+            response = requests.post("https://fake-news-detector-service-107273417489.us-central1.run.app/predict", json={"title": title, "text": text})
             if response.status_code == 200:
                 result = response.json()["prediction"]
                 st.success(f"🧠 Prediction: **{result}**")
